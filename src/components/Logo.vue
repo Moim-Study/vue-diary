201fca3b0b66a9552ca9f3f2c9097f0957e4d959
eunjin0212 <eunjin9639@gmail.com>
2021-09-19 14:00:29 +0900

    Install sass-loader

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,30 +1,14 @@
 <template>
-  <div><img alt="Vue logo" src="../assets/logo.png" />iary</div>
+  <div><img alt="Vue logo" src="../assets/logo.png" /></div>
 </template>
 
 <script>
   export default {
     name: 'Viary',
-    props: {
-      title: String,
-    },
   };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
-  }
+<style lang="scss" scoped>
+  // acolor: #42b983;
 </style>
