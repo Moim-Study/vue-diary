353b125426d4a0b1684e2edd08a43e8ee9161593
eunjin0212 <eunjin9639@gmail.com>
2021-09-24 19:45:09 +0900

    reset index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Main from '../views/Main.vue';
 
-const routes = [
-  {
-    path: '/',
-    name: 'Main',
-    component: Main,
-  },
-  {
-    path: '/timeline',
-    name: 'timeline',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/TimeLine.vue'),
-  },
-  {
-    path: '/todo',
-    name: 'Todo',
-    component: () => import('../views/Todo.vue'),
-  },
-];
+const routes = [];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
