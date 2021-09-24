4fe84b99e259fcbedef2542a9ee20c66c6141108
eunjin0212 <eunjin9639@gmail.com>
2021-09-24 21:21:11 +0900

    Modified Loading SCSS

--- src/views/Loading.vue
+++ src/views/Loading.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="logo">
-    <img alt="Vue logo" src="../assets/logo.png" /><span>iary</span>
+  <div class="loadingContainer">
+    <div class="loading">
+      <img alt="Vue logo" src="../assets/logo.png" /><span>iary</span>
+    </div>
   </div>
 </template>
 
@@ -13,19 +15,30 @@
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
   // color: #42b983;
-  .logo {
+  .loadingContainer {
     width: 100vw;
     height: 100vh;
-    span {
-      font: {
-        family: 'Black Han Sans', sans-serif;
-        weight: 900;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    .loading {
+      position: relative;
+      span {
+        position: absolute;
+        bottom: 10%;
+        right: 6%;
+        font: {
+          family: 'Black Han Sans', sans-serif;
+          weight: 900;
+          size: 30px;
+        }
       }
-    }
-    img {
-      width: 15%;
-      height: auto;
-      box-sizing: border-box;
+      img {
+        width: 65%;
+        height: auto;
+        box-sizing: border-box;
+      }
     }
   }
 </style>
