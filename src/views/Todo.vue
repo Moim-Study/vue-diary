9463b423bba85e20131d9ee1e73c9eed500a5841
eunjin0212 <eunjin9639@gmail.com>
2021-09-24 22:46:47 +0900

    Add Todo icon

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,22 +1,30 @@
 <template>
-  <div>
-    <h2>Goal</h2>
+  <div class="todoContainer">
+    <span class="todoTitle">Goal</span>
     <progress :max="todoArray.length" :value="doneArray.length">{{
       doneArray.length
     }}</progress>
   </div>
-  <div>
-    <h2>Todo</h2>
-    <div v-for="(i, j) in todoArray" :key="j">
-      <span>{{ i.todo }}</span>
+  <div class="todoContainer">
+    <span class="todoTitle">Todo</span>
+    <div class="todoContents" v-for="(i, j) in todoArray" :key="j">
       <span>{{ i.primary }}</span>
+      <span>{{ i.todo }}</span>
+      <span>
+        <img src="../assets/todo/edit.svg" />
+        <img src="../assets/todo/check-circle.svg" />
+        <img src="../assets/todo/trash.svg" />
+      </span>
     </div>
   </div>
-  <div>
-    <h2>Done</h2>
-    <div v-for="(i, j) in doneArray" :key="j">
-      <span>{{ i.todo }}</span>
+  <div class="todoContainer">
+    <span class="todoTitle">Done</span>
+    <div class="todoContents doneContents" v-for="(i, j) in doneArray" :key="j">
       <span>{{ i.primary }}</span>
+      <span
+        ><s>{{ i.todo }}</s></span
+      >
+      <span><img src="../assets/todo/trash.svg"/></span>
     </div>
   </div>
 </template>
@@ -43,4 +51,51 @@
   };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  @import '../Mixin.scss';
+  .todoContainer {
+    @include flex(center, center, column);
+    margin-bottom: 30px;
+    .todoTitle {
+      width: 100vw;
+      font: {
+        size: 20px;
+        weight: 500;
+        family: 'Roboto Mono', monospace;
+      }
+    }
+    .todoContents {
+      @include flex(space-between, center, row);
+      width: 70%;
+      span {
+        display: inline-block;
+        &:first-of-type {
+          width: 5%;
+          font-size: 10px;
+          line-height: 2.5;
+        }
+        &:nth-of-type(2) {
+          width: 65%;
+          text: {
+            align: left;
+            indent: 10px;
+          }
+        }
+        &:last-of-type {
+          width: 30%;
+          text-align: right;
+          img {
+            width: 20px;
+            height: 20px;
+            object-fit: contain;
+            margin-left: 10%;
+            cursor: pointer;
+            &:first-of-type {
+              margin-left: 0;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
