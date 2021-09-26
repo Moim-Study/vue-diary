8d636439f845dfa55a42627ddda4e3623f176c8f
mis4828 <ak3332@naver.com>
2021-09-26 11:50:22 +0900

    commit

---
+++ src/views/Diary.vue
@@ -0,0 +1,14 @@
+<template>
+<div>
+    <section>
+        <div>dasfdasf</div>    
+    </section>
+</div>
+</template>
+<script>
+import diaryModal from '../components/ModalDiary.vue'
+export default {
+    name: 'diary'
+    
+}
+</script>
